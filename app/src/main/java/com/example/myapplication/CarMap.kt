3d15248ef9fc52841f12d6b5f261434ddb8d5928
phjin0628 <phjin0628@naver.com>
2021-04-29 02:31:43 +0900

    카드뷰 상세정보 완성

--- app/src/main/java/com/example/myapplication/CarMap.kt
+++ app/src/main/java/com/example/myapplication/CarMap.kt
@@ -1,13 +1,15 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Transformations.map
 import com.example.myapplication.mapdata.CarMapJson
-
+import com.example.myapplication.mapdata.Ddu
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,12 +19,14 @@ import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_car_map.*
+import kotlinx.android.synthetic.main.activity_car_map.view.*
 import okio.Okio.sink
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class CarMap : AppCompatActivity(), OnMapReadyCallback {
 
@@ -68,14 +72,20 @@ class CarMap : AppCompatActivity(), OnMapReadyCallback {
 
             val position=LatLng(car.latitude.toDouble(),car.longitude.toDouble()) //position은 위도 경도
 
-            val marker=MarkerOptions()
+            val marker=MarkerOptions() //MarkerOptions에는 기본적으로 title 이나 snippet 이외에는 값을 담을 수 없음
                     .position(position)
                     .title(car.Name)
-            mMap.addMarker(marker)
+
+            //mMap.addMarker(marker)은 해주면 안된다 //val marker=MarkerOptions() 두번 겹쳐서 실행이 안됐음
             latLngBounds.include(marker.position)
 
-
+            val marker1: Marker = mMap.addMarker(marker) //
+            marker1.tag =
+                    "${car.description}/${car.toilet}/${car.sink}/${car.tap_water}" //${}하나의 배열이라고 생각하면 된다
         }
+
+
+
         val positionss=LatLng(37.20917443124407, 126.976336711454)
         val cameraUpdate = CameraUpdateFactory.newLatLngZoom(positionss,10.0F)
         mMap.moveCamera(cameraUpdate) //(updated)
@@ -90,17 +100,15 @@ class CarMap : AppCompatActivity(), OnMapReadyCallback {
                 var carmaptoilet = findViewById<TextView>(R.id.toilet)
                 var carmapsink = findViewById<TextView>(R.id.sink)
                 var carmaptap_water = findViewById<TextView>(R.id.tap_water)
-
-                /*
-                var arr = marker.tag.toString().split("/") //마커에 붙인 태그
                 carmapname.text = marker.title
-                carmapdescription.text = marker.snippet
-                carmaptoilet.text = arr[0]
-                carmapsink.text = arr[1]
-                carmaptap_water.text = arr[2]
-                 */
 
-                return false //잘해보자
+                val tempArray = (marker.tag as String).split("/") //스택 오버플로를 막기 위해 /로 구분되게 배열로 나눈 것
+                carmapdescription.text = tempArray[0]
+                carmaptoilet.text = tempArray[1]
+                carmapsink.text = tempArray[2]
+                carmaptap_water.text = tempArray[3]
+
+                return false//잘해보자
             }
         })
         mMap!!.setOnMapClickListener(object : GoogleMap.OnMapClickListener {
