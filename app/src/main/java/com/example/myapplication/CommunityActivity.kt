0869f4c48c2255eb7463496930a399cd4e3d2ecf
qusrmsdud77@naver.com <qusrmsdud77@naver.com>
2021-05-05 14:12:30 +0900

    title_hatgalryeo_ssibul

--- app/src/main/java/com/example/myapplication/CommunityActivity.kt
+++ app/src/main/java/com/example/myapplication/CommunityActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityCategoryBinding
+import com.example.myapplication.databinding.ActivityCommunityBinding
 
 class CommunityActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivityCommunityBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community)
+
+        val intent = Intent (this, PublicActivity::class.java)
+        binding.btnPublic.setOnClickListener { startActivity(intent) }
     }
 }
