1c020c966df07efaa4b0c18217282434d96ab746
qusrmsdud77@naver.com <qusrmsdud77@naver.com>
2021-05-07 19:59:26 +0900

    로그인

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -4,27 +4,86 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.core.widget.addTextChangedListener
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.myapplication.databinding.ActivityLoginBinding
+import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
 class LoginActivity : AppCompatActivity() {
-//로그인 버튼 누르면 카테고리 화면으로 이동, 로그인 구현하고 db랑 일치해야 넘어가는걸로 수정 해야함
+
+    lateinit var id:EditText
+    lateinit var password:EditText
+    lateinit var button:Button
+    lateinit var btnRegister:Button
+
     val binding by lazy{ ActivityLoginBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_login) //binding.root
 
+        id=findViewById(R.id.textid)
+        password=findViewById(R.id.textPassword)
+        button=findViewById(R.id.loginBtn)
+        btnRegister=findViewById(R.id.btnRegister)
+
+
+        val retrofit=Retrofit.Builder()
+            .baseUrl("http://3.34.141.115")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+            btnRegister.setOnClickListener{
+            Intent(this,SignUpActivity::class.java).run{
+                   startActivity(this)
+            }
+
+         /*
+          val binding by lazy{ ActivityLoginBinding.inflate(layoutInflater)
+
+            val signup= Intent(this, SignUpActivity::class. java)
+            binding.btnRegister.setOnClickListener{ startActivity(signup)}
+*/
+        val service=retrofit.create(SignService::class.java)
+        button.setOnClickListener {
+            val idStr=id.text.toString()
+            val pwStr=password.text.toString()
+            service.login(idStr,pwStr).enqueue(object : Callback<LoginResponse> {
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                   val result=response.body()
+                    Log.d("로그인","${result}")
+                }
+
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Log.e("로그인","${t.localizedMessage}")
+                }
+            })
+        }
+
+
+        }
+
+
+        /*
         val intent= Intent(this, CategoryActivity::class.java)
         binding.loginBtn.setOnClickListener{ startActivity(intent) }
 
         val signup= Intent(this, SignUpActivity::class. java)
         binding.signupBtn.setOnClickListener{ startActivity(signup)}
 
-//p.254 글자 실시간으로 처리하기 일단 구현해봤는데 필요없으면 삭제 (아이디 부분만 해당)
-        binding.id.addTextChangedListener {
-            Log.d("EditText", "현재 입력된 값=${it.toString()}")
-        }
-    }
-
+         */
 }
+
+
+interface SignService{ //레트로핏을 활용하기 위한 코드
+    @FormUrlEncoded //@Field 필드 값을 받기 위해 선언해줘야함
+    @POST("login.php")
+    fun login(@Field("id")id:String,@Field("password")pw:String): Call<LoginResponse>
+}}
